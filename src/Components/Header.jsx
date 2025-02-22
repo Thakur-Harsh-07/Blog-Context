fced98c1943c0f6f32525dd7c23af53e4ce2ae21
Thakur-Harsh-07 <harshchauhan7081@gmail.com>
2025-02-22 16:55:28 +0530

    Project Added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full border shadow-md py-3 fixed top-0 bg-white'>
+        <header className='text-center font-serif' >
+            <h1 className='font-bold text-3xl uppercase'>CodeHelp Blogs</h1>
+        </header>
+    </div>
+  )
+}
+
+export default Header
